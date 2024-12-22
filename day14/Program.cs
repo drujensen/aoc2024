b8893315fad0331f2c57f487aad21fe64acbc74d
Dru Jensen <drujensen@gmail.com>
2024-12-22 14:40:10 -0800

    feat: day 14

--- day14/Program.cs
+++ day14/Program.cs
@@ -1,4 +1,6 @@
-﻿class Robot
+﻿using System.Security.Cryptography.X509Certificates;
+
+class Robot
 {
     public int PX { get; set; }
     public int PY { get; set; }
@@ -29,7 +31,7 @@
         if (PY >= height)
             PY = PY - height;
     }
-    
+
 }
 
 class HeadQuarters
@@ -54,27 +56,90 @@ class HeadQuarters
             {
                 robot.Move(width, height);
             }
+            
+            if (FindTree())
+            {
+                Display(i);
+            }
+        }
+    }
+
+    public bool FindTree()
+    {
+        var left = 0;
+        var right = 0;
+        var midWidth = (width -1) / 2;
+        
+        foreach (var robot in robots)
+        {
+            if (robot.PX < midWidth)
+            {
+                left += robot.PX * robot.PY;
+            }
+            else if (robot.PX > midWidth)
+            {
+                right += ((width - 1) - robot.PX) * robot.PY;
+            }
+        }
+
+        
+        if (left == right)
+        {
+            return true;
+        }
+        
+        return false;
+    }
+
+    public void Display(int cnt)
+    {
+        Console.WriteLine($"After {cnt} seconds");
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (robots.Any(r => r.PX == j && r.PY == i))
+                {
+                    Console.Write("#");
+                }
+                else
+                {
+                    Console.Write(".");
+                }
+            }
+            Console.WriteLine();
         }
     }
 
     public void Print()
     {
-        Move(100);
+        Move(10000000);
 
         var quadrant1 = 0;
         var quadrant2 = 0;
         var quadrant3 = 0;
         var quadrant4 = 0;
+        var midWidth = width / 2;
+        var midHeight = height / 2;
         foreach (var robot in robots)
         {
-            if (robot.PX < width / 2 && robot.PY < height / 2)
+            if (robot.PX < midWidth && robot.PY < midHeight)
+            {
                 quadrant1++;
-            else if (robot.PX >= width / 2 && robot.PY < height / 2)
+            }
+            else if (robot.PX > midWidth && robot.PY < midHeight)
+            {
                 quadrant2++;
-            else if (robot.PX < width / 2 && robot.PY >= height / 2)
+            }
+            else if (robot.PX < midWidth && robot.PY > midHeight)
+            {
                 quadrant3++;
-            else if (robot.PX >= width / 2 && robot.PY >= height / 2)
+            }
+            else if (robot.PX > midWidth && robot.PY > midHeight)
+            {
                 quadrant4++;
+            }
+            
         }
         Console.WriteLine($"Q1: {quadrant1}, Q2: {quadrant2}, Q3: {quadrant3}, Q4: {quadrant4}");
         Console.WriteLine($"Total: {quadrant1 * quadrant2 * quadrant3 * quadrant4}");
